a4caf0883bc0e2ff7f3e373d92b27caaedebfe4d
FilipWarpman <Filip.warpman@gmail.com>
2022-04-28 20:00:41 +0200

    Added WeatherAPI in JS and updated CSS correlating to API

--- PubCrawlerClient/src/main/webapp/js/PubCrawlerJs.js
+++ PubCrawlerClient/src/main/webapp/js/PubCrawlerJs.js
@@ -334,7 +334,9 @@ function clicked() {
 	button.classList.toggle('active');
 }
 
-function ParseJsonFileWeather(result) {
+
+
+/*function ParseJsonFileWeather(result) {
 	var lat = result.latitude;
 	var long = result.longitude;
 	var city = result.city;
@@ -367,5 +369,57 @@ function ParseJsonFileWeather(result) {
 
 	function ajaxWeatherReturn_Error(result, status, xhr) {
 		console.log("API-Weather: " + status);
-	}
-}
+	}*/
+	       $(document).ready(function () {
+        $.ajax({
+            method: "GET",
+            url: "http://api.ipstack.com/84.55.116.250?access_key=4a2afa8f74c876cac2ce672cbf42b8e4",
+             error: ajaxReturn_Error,
+            success: ajaxReturn_Success
+        })
+        function ajaxReturn_Success(result, status, xhr) {
+            ParseJsonFile(result);
+        }
+        function ajaxReturn_Error(result, status, xhr) {
+            console.log("Ajax-api-stack: " + status);
+        }
+    });//End ready function 
+    function ParseJsonFile(result) {
+        var lat = result.latitude;
+        var long = result.longitude;
+        var city = result.city;
+        var ipNbr = result.ip
+        $("#city").text(city);
+        $("#ipNbr").text(ipNbr);
+        $.ajax({
+            method: "GET",
+            url:
+                "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units= metric" + "&APPID=d16e8687b228762ca06d991d5e25e465",
+                error: ajaxWeatherReturn_Error,
+            success: ajaxWeatherReturn_Success
+        })
+
+        function ajaxWeatherReturn_Success(result, status, xhr) {
+            var sunrise = result.sys.sunrise;
+            var sunset = result.sys.sunset;
+
+            var sunriseDate = new Date(sunrise * 1000);
+            var timeStrSunrise = sunriseDate.toLocaleTimeString("sv-SE");
+            var sunsetDate = new Date(sunset * 1000);
+            var timeStrSunset = sunsetDate.toLocaleTimeString("sv-SE");
+
+            $("#sunrise").text("Sunrise: " + timeStrSunrise);
+            $("#sunset").text("Sunset: " + timeStrSunset);
+
+            $("#weather").text(result.weather[0].main);
+
+            $("#degree").text(result.main.temp + " \u2103");
+        }//ajaxWeatherReturn_Success 
+
+        function ajaxWeatherReturn_Error(result, status, xhr) {
+            alert("Error i OpenWeaterMap Ajax");
+            console.log("Ajax-find movie: " + status);
+        }
+    }
+	
+
